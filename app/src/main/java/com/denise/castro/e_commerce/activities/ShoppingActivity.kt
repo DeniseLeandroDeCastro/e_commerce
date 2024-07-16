2ed053f193bd1f46587ebb2ee7da65e72768ef10
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2024-07-16 19:54:17 -0300

    16.07.2024 -> Adiciona menu e bottom navigation e ShoppingActivity

--- app/src/main/java/com/denise/castro/e_commerce/activities/ShoppingActivity.kt
+++ app/src/main/java/com/denise/castro/e_commerce/activities/ShoppingActivity.kt
@@ -3,14 +3,23 @@ package com.denise.castro.e_commerce.activities
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.denise.castro.e_commerce.R
+import com.denise.castro.e_commerce.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/denise/castro/e_commerce/fragments/shopping/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.denise.castro.e_commerce.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.denise.castro.e_commerce.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
