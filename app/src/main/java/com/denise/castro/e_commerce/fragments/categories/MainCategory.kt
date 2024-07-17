670486f0509f07990be00148afa41e42d5824b13
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2024-07-16 21:06:37 -0300

    16.07.2024 -> Adiciona Fargment Category

---
+++ app/src/main/java/com/denise/castro/e_commerce/fragments/categories/MainCategory.kt
@@ -0,0 +1,7 @@
+package com.denise.castro.e_commerce.fragments.categories
+
+import androidx.fragment.app.Fragment
+import com.denise.castro.e_commerce.R
+
+class MainCategory: Fragment(R.layout.fragment_main_category) {
+}
--- app/src/main/java/com/denise/castro/e_commerce/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/denise/castro/e_commerce/fragments/shopping/HomeFragment.kt
@@ -1,7 +1,31 @@
 package com.denise.castro.e_commerce.fragments.shopping
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.denise.castro.e_commerce.R
+import com.denise.castro.e_commerce.databinding.FragmentHomeBinding
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val categoriesFragments = arrayListOf<Fragment>(
+
+        )
+    }
 }
